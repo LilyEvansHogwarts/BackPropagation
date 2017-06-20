1ce04b4e54b02ac8b5b06e7fa7ecc441267f7fb5
LilyEvansHogwarts <825196119@qq.com>
2017-06-20 12:33:59 +0800

    softmax vs softmax loss

--- fullconnected/README.md
+++ fullconnected/README.md
@@ -1,4 +1,4 @@
-fullyconnected layer
+# fullyconnected layer
 
 ## 构造A.mat之类的文件
 
@@ -14,6 +14,10 @@ image_deleted.sh
 
 construct_data_structure.m作为单独的文件存在，是为了降低网络的工作量，可以单独运行construct_data_struct.m文件，这样以后就不用每次运行网络都要重新构建train、valid、test数据集了。
 
+## change_label.m
+
+在实际使用label的时候要求label为(1,num_classes)的shape，因此要提前对于train、test、valid数据集里的label进行处理。
+
 ## 运行fullyconnected.m和fullyconnected.m
 
 在运行fullyconnected.m和fullyconnected_batch.m的时候发现，采用单纯的sgd进行权重更新，所得到的最终的accuracy反而不低，采用batch的方法存在的问题是:
@@ -25,3 +29,8 @@ batch的方法要想得到和单样本sgd一样的精度需要更大的数据量
 在初次运行fullyconnected_two_layers.m对于fc1_output没有除去normal1和fc2_output除以normal2，以至于最终的网络运行结果精度为0.12，当对于fc2_output/normal2后，网络精度提升至0.30左右，添加对于fc1_output/normal1后，网络精度提升至0.60。(backpropagation不除以normal1和normal2，learning-rate不发生改变的情况下，train_dataset运行一遍跑出来的结果)
 不知出于什么原因，batch的运行精度要略小于单样本更新权重的最终精度。
 当将backpropagation中添加除以normal1和normal2的部分，发现将train_dataset运行20遍，最终的精度为0.54，基本达到极限，也就是说两层fc的分类精度为0.54，但对比前面的实验发现单层fc的精度反而更高。
+
+## fullyconnected_two_layers_softmax.m
+
+由于经过两层全连接层后，由于权重相乘后相加，最终x2 = x1 * w2 + b2的量级会达到立方量级，这会对于后续的softmax中exp计算带来困扰，因此造成softmax的overflow，初始的时候通过x2 / norm(x2)的方式来解决这一问题，发现最终的训练精度一直上不来，需要经过多轮训练才能够取得0.5以上的精度，但是将x2进行处理x2 - max(x2)之后，再采用softmax函数，也就是最终的softmax结果并没有发生改变，再进行训练发现训练精度在pass = 1的情况下基于能够的得到大幅度提升最终为0.78。
+反思原因: 在对于x2 / norm(x2)处理的过程中，其实是变相的改变了两层全连接层的实际分类结果，与原有产生一定程度上的偏差，这也就带来权重更新的不准确，但是x2 - max(x2)的预处理并不会带来softmax的分类结果的改变，因此很好的保留了原有分类器的分类结果，权重更新上也不会造成强度偏差。具体可以参考:![softmax-vs-softmax-loss](freemid.pluskid.org/machine-learning/softmax-vs-softmax-loss-numerical-stability/)相应的pdf为![softmax-vs-softmax-loss-numerical-stability](softmax-vs-softmax-loss-numerical-stability.pdf)
---
+++ fullconnected/fullyconnected_two_layers_softmax.m
@@ -0,0 +1,70 @@
+%load('train_dataset.mat');
+%load('train_label.mat');
+
+
+train_size = 200000;
+valid_size = 10000;
+test_size = 10000;
+num_classes = 10;
+image_size = 28;
+num_hidden = 64;
+
+%train_dataset = reshape(train_dataset,train_size,image_size * image_size);
+
+fc1_weights = rand(image_size * image_size, num_hidden);
+fc1_biases = rand(1, num_hidden);
+fc2_weights = rand(num_hidden, num_classes);
+fc2_biases = rand(1, num_classes);
+%load('fc2_weights.mat');
+%load('fc2_biases.mat');
+%load('fc1_weights.mat');
+%load('fc1_biases.mat');
+learning_rate = 0.5;
+loss_sum = 0;
+accuracy = 0;
+pass = 1;
+for j = 1:pass
+	for i = 1:train_size
+		fc1_output = train_dataset(i, :, :) * fc1_weights + fc1_biases;
+		normal1 = norm(fc1_output);
+		fc2_input = fc1_output / normal1;
+		fc2_output = fc2_input * fc2_weights + fc2_biases;
+		normal2 = norm(fc2_output);
+		%logits = softmax(fc2_output / normal2);
+		logits = softmax(fc2_output - max(fc2_output));
+		loss = sum((train_label(i,:) - logits) .* (train_label(i,:) - logits)) / 2;
+		loss_sum = loss_sum + loss;
+		[arg,argmax_logits] = max(logits);
+		[arg,argmax_label] = max(train_label(i,:));
+		accuracy = accuracy + (argmax_logits == argmax_label);
+		if mod(i,500) == 0
+			loss_sum
+			accuracy = accuracy / 500
+			loss_sum = 0;
+			accuracy = 0;
+		end
+		derive_fc2_biases = (logits - train_label(i,:)) .* logits .* (1 - logits); %/ normal2;
+		derive_fc2_weights = fc2_input' * derive_fc2_biases;
+		derive_fc1_biases = derive_fc2_biases * fc2_weights' / normal1;
+		derive_fc1_weights = train_dataset(i,:,:)' * derive_fc1_biases;
+		fc2_weights = fc2_weights - learning_rate * derive_fc2_weights;
+		fc2_biases = fc2_biases - learning_rate * derive_fc2_biases;
+		fc1_weights = fc1_weights - learning_rate * derive_fc1_weights;
+		fc1_biases =fc1_biases - learning_rate * derive_fc1_biases;
+	end
+	if mod(j,4) == 0
+		learning_rate = learning_rate * 0.707;
+	end
+end
+
+accuracy = 0;
+for i = 1:train_size
+	fc2_input = train_dataset(i,:,:) * fc1_weights + fc1_biases;
+	fc2_output = fc2_input * fc2_weights + fc2_biases;
+	normal = norm(fc2_output);
+	logits = softmax(fc2_output / normal);
+	[arg, argmax_logits] = max(logits);
+	[arg, argmax_label] = max(train_label(i,:));
+	accuracy = accuracy + (argmax_logits == argmax_label);
+end
+train_accuracy = accuracy / train_size
